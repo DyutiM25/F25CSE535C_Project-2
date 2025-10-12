464d2b1912470acc2129884efab2d10bf5db1903
Aditya Toleti Sai Venkata <atoletis@asu.edu>
2025-10-12 16:16:18 -0700

    Add skeleton functions for game logic and data storage

---
+++ app/src/main/java/com/example/tictaktoeproject2/DataStorage.kt
@@ -0,0 +1,13 @@
+package com.example.tictaktoeproject2
+
+import android.content.Context
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun saveGameRecord(context: Context, winner: Player?, difficulty: Difficulty) {
+    //Write code for local db storing after game is over
+}
+
+fun loadGameRecords(context: Context): List<GameRecord> {
+    return emptyList()
+}
---
+++ app/src/main/java/com/example/tictaktoeproject2/GameLogic.kt
@@ -0,0 +1,20 @@
+package com.example.tictaktoeproject2
+
+import kotlin.random.Random
+
+fun checkWinner(board: Array<Player?>): Pair<Player?, String> {
+    return Pair(null, "")
+}
+
+fun getRandomMove(board: Array<Player?>): Int {
+    return -1
+}
+
+fun getBestMove(board: Array<Player?>): Int {
+    return -1
+}
+
+fun minimax(board: Array<Player?>, depth: Int, isMaximizing: Boolean): Int {
+    return 0
+}
+
